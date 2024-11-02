f1290fadf82604c24c494045c1d664106bc44d09
An Nguyen <82162492+phucannguyen-dev@users.noreply.github.com>
2024-11-02 18:50:23 +0700

    Update unora.ino

--- parking_v0/unora.ino
+++ parking_v0/unora.ino
@@ -7,15 +7,15 @@
 #define SS_PIN  10 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Đối tượng điều khiển module RFID
-String card[13] = {"2ADBF93D", "B18BFC3D", "44DCF93D", "9C93F93D", "D3F4F93D", 
+String cards[13] = {"2ADBF93D", "B18BFC3D", "44DCF93D", "9C93F93D", "D3F4F93D", 
                    "EFF2FB3D", "CFF2FB3D", "508CFC3D", "3322FC3D", "135D6A19", 
                    "13FD7CE2", "23837DE4", "A965FC3D"};
-String tagID = "";
+String tagId = "";
 
-Servo servora;  // Servo để mở cửa
+Servo exitServo;  // Servo để mở cửa
 
-int exitStat = 0;
-int enterStat = 0;
+int exitStatus = 0;
+int enterStatus = 0;
 
 void setup() {
     Serial.begin(9600);  // Khởi tạo giao tiếp Serial
@@ -23,26 +23,26 @@ void setup() {
     SPI.begin();  // Khởi tạo giao tiếp SPI
     mfrc522.PCD_Init();  // Khởi tạo module RFID
 
-    servora.attach(9);  // Gán servo vào chân số 3
-    servora.write(0);   // Đưa servo về vị trí 0 độ (đóng cửa)
+    exitServo.attach(9);  // Gán servo vào chân số 3
+    exitServo.write(0);   // Đưa servo về vị trí 0 độ (đóng cửa)
 
     Serial.println("Hello2");  // In ra thông báo khởi động thành công
 }
 
 void loop() {
-    if (getID()) {
-        bool cardFound = false;
+    if (isCardDetected()) {
+        bool isCardFound = false;
         for (int i = 0; i < 13; i++) {
-            if (tagID == card[i]) {
+            if (tagId == cards[i]) {
                 Serial.print("Card detected, UID: ");
-                Serial.println(tagID);
-                opengate();
-                cardFound = true;
+                Serial.println(tagId);
+                openGate();
+                isCardFound = true;
                 break;
             }
         }
 
-        if (!cardFound) {  // Nếu không tìm thấy thẻ hợp lệ
+        if (!isCardFound) {  // Nếu không tìm thấy thẻ hợp lệ
             Serial.println("No card found");
             delay(100);
         }
@@ -50,15 +50,15 @@ void loop() {
 }
 
 // Hàm mở cửa
-void opengate() {
-    servora.write(90);  // Xoay servo 90 độ (mở cửa)
+void openGate() {
+    exitServo.write(90);  // Xoay servo 90 độ (mở cửa)
     delay(2000);         // Giữ cửa mở trong 2 giây
-    servora.write(0);   // Đóng cửa lại
+    exitServo.write(0);   // Đóng cửa lại
 }
 
 
 // Hàm đọc và lưu UID của thẻ RFID
-boolean getID() {
+boolean isCardDetected() {
     // Kiểm tra xem có thẻ mới không
     if (!mfrc522.PICC_IsNewCardPresent()) {
         return false;
@@ -74,9 +74,9 @@ boolean getID() {
     for (uint8_t i = 0; i < 4; i++) {
         sprintf(&uidStr[i * 2], "%02X", mfrc522.uid.uidByte[i]);  // Lưu từng byte UID vào chuỗi dạng HEX
     }
-    tagID = String(uidStr);  // Chuyển thành String để sử dụng
+    tagId = String(uidStr);  // Chuyển thành String để sử dụng
 
-    tagID.toUpperCase();  // Đưa chuỗi thành chữ hoa
+    tagId.toUpperCase();  // Đưa chuỗi thành chữ hoa
 
     mfrc522.PICC_HaltA();  // Dừng việc đọc thẻ
 
